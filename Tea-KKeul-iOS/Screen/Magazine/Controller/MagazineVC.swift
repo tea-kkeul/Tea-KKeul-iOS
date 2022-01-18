ba8739a03237cd44683140ce5017ad4c4b72fb31
Hwang <dudhkd98@naver.com>
2022-01-19 01:37:25 +0900

    [FEAT]: MagazineWebView-open as Safari in app

--- Tea-KKeul-iOS/Screen/Magazine/Controller/MagazineVC.swift
+++ Tea-KKeul-iOS/Screen/Magazine/Controller/MagazineVC.swift
@@ -6,15 +6,16 @@
 //
 
 import UIKit
+import SafariServices
 
 class MagazineVC: UIViewController {
     let DummyMagazineData = [
-        ["magazine1", "지나친 기대도 실망도 없는 영국 차문화"],
-        ["magazine2", "중국 Tea 파헤치기"],
-        ["magazine3", "Tea 웨이의 브런치"],
-        ["magazine4", "습식다법과 건식다법의 차이"],
-        ["magazine5", "차를 오래 보관하려면"],
-        ["magazine6", "차 제대로 마시는 법"],
+        ["magazine1", "지나친 기대도 실망도 없는 영국 차문화","https://brunch.co.kr/@beansj/228"],
+        ["magazine2", "중국 Tea 파헤치기","https://brunch.co.kr/magazine/exploring-tea"],
+        ["magazine3", "Tea 웨이의 브런치", "https://brunch.co.kr/@manitea#articles"],
+        ["magazine4", "습식다법과 건식다법의 차이", "https://brunch.co.kr/@timetotea/51"],
+        ["magazine5", "차를 오래 보관하려면", "https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=pko1127&logNo=220963488655"],
+        ["magazine6", "차 제대로 마시는 법", "https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=nobia10004&logNo=220576482500"],
     ]
     
     @IBOutlet weak var titleView: UIView!
@@ -41,6 +42,15 @@ extension MagazineVC {
         
         magazineCV.backgroundColor = .white
     }
+    // 앱 내에서 Safari Open
+    func openSafari(with url: URL?) {
+      guard let url = url else { return }
+
+      let config = SFSafariViewController.Configuration()
+      let vc = SFSafariViewController(url: url, configuration: config)
+
+      present(vc, animated: true)
+    }
 }
 
 //MARK: UICollectionViewDataSource
@@ -64,7 +74,11 @@ extension MagazineVC: UICollectionViewDataSource {
 
 //MARK: UICollectionViewDelegate
 extension MagazineVC: UICollectionViewDelegate {
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let url = URL(string: "\(DummyMagazineData[indexPath.row][2])")
+        
+        openSafari(with: url!)
+    }
 }
 
 //MARK:
