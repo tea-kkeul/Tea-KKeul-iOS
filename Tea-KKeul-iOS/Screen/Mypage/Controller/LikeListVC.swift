7d119d5ce71f3809f8f95fc2e606b03d0e21c996
Hwang <dudhkd98@naver.com>
2022-01-19 22:57:24 +0900

    [FEAT]: navigation setting

---
+++ Tea-KKeul-iOS/Screen/Mypage/Controller/LikeListVC.swift
@@ -0,0 +1,18 @@
+//
+//  LikeListVC.swift
+//  Tea-KKeul-iOS
+//
+//  Created by 황윤경 on 2022/01/19.
+//
+
+import UIKit
+
+class LikeListVC: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
+//MARK: Custom Method
+extension LikeListVC {
+
+}
