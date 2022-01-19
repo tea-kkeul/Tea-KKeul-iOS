af9f77f165b3652446911d24fca974e881276d22
Hwang <dudhkd98@naver.com>
2022-01-19 21:29:45 +0900

    [FEAT]: address 임시 데이터

--- Tea-KKeul-iOS/Screen/Mypage/Controller/DeliveryListVC.swift
+++ Tea-KKeul-iOS/Screen/Mypage/Controller/DeliveryListVC.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class DeliveryListVC: UIViewController {
+    let AddressData = [
+        ["집", ["서울특별시 노원구 공릉로 20번길 15-3(공릉동)", "티끌아파트 506동 1203호", "(02708)"], "김슈니", "010-1234-5678"],
+        ["회사", ["서울특별시 종로구 세종대로 178(종로3가동)", "트윈빌딩 A동 605호", "(04509)"], "김슈니", "010-1234-5678"]
+    ]
     @IBOutlet weak var addressListCV: UICollectionView!
     override func viewDidLoad() {
         setNaviBar()
@@ -29,7 +33,7 @@ extension DeliveryListVC {
 //MARK: UICollectionViewDataSource
 extension DeliveryListVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return AddressData.count + 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
