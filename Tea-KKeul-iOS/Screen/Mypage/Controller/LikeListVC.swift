9e6931670fe359acf0268ef84e9e9be21773377e
Hwang <dudhkd98@naver.com>
2022-01-19 23:26:33 +0900

    [FEAT]: LikeListCVC

--- Tea-KKeul-iOS/Screen/Mypage/Controller/LikeListVC.swift
+++ Tea-KKeul-iOS/Screen/Mypage/Controller/LikeListVC.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class LikeListVC: UIViewController {
     override func viewDidLoad() {
-        
+        setNaviBar()
     }
 }
 //MARK: Custom Method
 extension LikeListVC {
-
+    func setNaviBar() {
+        navigationItem.title = "찜 목록"
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.darkGreen]
+    }
 }
