3612ac5ef611a2aad5ec5f227fa6937c6e5cbd0d
Hwang <dudhkd98@naver.com>
2022-01-19 17:16:19 +0900

    [FEAT]: constraint, detail

--- Tea-KKeul-iOS/Screen/Mypage/Controller/MypageVC.swift
+++ Tea-KKeul-iOS/Screen/Mypage/Controller/MypageVC.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class MypageVC: UIViewController {
+    let UserData = ["김슈니", "서울시 공릉 1동 20번길 15-3"]
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var userAddress: UILabel!
     
     @IBOutlet weak var boxView: UIView!
     
     override func viewDidLoad() {
+        setTitleView()
+    }
+}
+//MARK: Custom Method
+extension MypageVC {
+    func setTitleView() {
+        userName.text = UserData[0]
+        userAddress.text = UserData[1]
         
+        boxView.layer.cornerRadius = 10
     }
 }
