271b708e4adac4b1cf8a5b456862924fa45879b2
naerim <naerim1119@gmail.com>
2022-01-19 21:17:58 +0900

    [Update] : apple login -ing

--- Tea-KKeul-iOS/Screen/Login/Controller/LoginVC.swift
+++ Tea-KKeul-iOS/Screen/Login/Controller/LoginVC.swift
@@ -5,15 +5,19 @@
 //  Created by 김내림 on 2022/01/18.
 //
 
+import AuthenticationServices
+import CryptoKit
 import Firebase
+import FirebaseAuth
 import GoogleSignIn
 import UIKit
 
 class LoginVC: UIViewController {
-    
     @IBOutlet weak var googleLoginBtn: GIDSignInButton!
     @IBOutlet weak var appleLoginBtn: UIButton!
 
+    private var currentNonce: String?
+    
     let db = Firestore.firestore()
         
     override func viewDidLoad() {
@@ -28,6 +32,7 @@ class LoginVC: UIViewController {
     }
     
     @IBAction func appleLoginBtnClick(_ sender: Any) {
+        startSignInWithAppleFlow()
         print("apple")
     }
 }
@@ -51,7 +56,7 @@ extension LoginVC {
     }
 }
 
-// MARK: - function for login
+// MARK: - google for login
 extension LoginVC : GIDSignInDelegate{
     // 구글 로그인
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
@@ -76,12 +81,38 @@ extension LoginVC : GIDSignInDelegate{
             }
         }
     }
-    
-    // 메인 페이지로 이동
-    func goHome() {
-        let homeVC = ViewControllerFactory.viewController(for: .tabBar)
-        homeVC.modalPresentationStyle = .fullScreen
-        self.present(homeVC, animated: false, completion: nil)
+}
+
+// MARK: - function for apple login
+@available(iOS 13.0, *)
+extension LoginVC : ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {fatalError("error - no login request")}
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("unable to fetch token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else { return }
+            let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            // Signin with firebase
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                if (error != nil) {
+                    print(error!)
+                    return
+                }
+                let email = Auth.auth().currentUser?.email
+                let uid = authResult!.user.uid
+                print(uid)
+                self.db.collection("users").document(uid).setData([
+                    "uid" : uid,
+                    "email" : email as Any
+                ])
+                print(email!)
+                // goHome()
+                
+            }
+        }
     }
 }
 
@@ -96,6 +127,76 @@ extension UIView {
             self.layer.borderColor = newValue.cgColor
         }
     }
+}
+
+// MARK: - Apple sign in
+extension LoginVC :  ASAuthorizationControllerPresentationContextProviding{
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window!
+    }
+    
+    // 메인 페이지로 이동
+    func goHome() {
+        let homeVC = ViewControllerFactory.viewController(for: .tabBar)
+        homeVC.modalPresentationStyle = .fullScreen
+        self.present(homeVC, animated: false, completion: nil)
+    }
+    
+    func startSignInWithAppleFlow() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    
+    }
+    
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        String(format: "%02x", $0)
+      }.joined()
+      return hashString
+    }
+    
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+      var result = ""
+      var remainingLength = length
+      while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+          var random: UInt8 = 0
+          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+          if errorCode != errSecSuccess {
+            fatalError(
+              "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+          }
+          return random
+        }
+        randoms.forEach { random in
+          if remainingLength == 0 {
+            return
+          }
+
+          if random < charset.count {
+            result.append(charset[Int(random)])
+            remainingLength -= 1
+          }
+        }
+      }
+      return result
+    }
+
 }
 
 //        guard let homeVC = self.storyboard?.instantiateViewController(withIdentifier: Identifiers.tabBarSB) else {
