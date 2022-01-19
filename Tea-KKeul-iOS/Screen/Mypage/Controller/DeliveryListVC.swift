a3f9b0c1bb27198c5a3a0c336fad49c0c1d9cf21
Hwang <dudhkd98@naver.com>
2022-01-19 18:27:34 +0900

    [CHORE]: tabBar root - mypageVC to mypageNC

---
+++ Tea-KKeul-iOS/Screen/Mypage/Controller/DeliveryListVC.swift
@@ -0,0 +1,14 @@
+//
+//  DeliveryListVC.swift
+//  Tea-KKeul-iOS
+//
+//  Created by 황윤경 on 2022/01/19.
+//
+
+import UIKit
+
+class DeliveryListVC: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
